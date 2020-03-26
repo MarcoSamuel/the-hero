bca3fcef97a511a4e2ffedef45cd83baf7b2dd80
MarcoSamuel <marcossamuelsin@gmail.com>
2020-03-26 19:48:47 -0300

    Finalizado web

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,71 @@
+import React, {useEffect, useState} from 'react';
+import { Link, useHistory} from 'react-router-dom';
+import './styles.css';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import api from '../../services/api';
+import logoImg from '../../assets/logo.svg';
+export default function Profile(){
+	const [incidents, setIncidents] = useState([]);
+	const ongId = localStorage.getItem('ongId');
+	const ongName = localStorage.getItem('ongName');
+	const history = useHistory();
+	useEffect(() => {
+		api.get('profile', {
+			headers:{
+				Authorization:ongId
+			}
+		}).then(res =>{
+			setIncidents(res.data);
+		})
+	},[ongId])
+
+	async function handleDeleteIncident(id){
+		try{
+			await api.delete(`incidents/${id}`, {
+				headers:{
+					Authorization:ongId,
+				}
+			});
+			setIncidents(incidents.filter(incident => incident.id !==id))
+		}catch(err){
+			alert("erro ao deletar caso, tente novamente");
+		}
+	}
+
+	function handleLogout(){
+		localStorage.clear();
+
+		history.push('/');
+	}
+
+	return (
+		<div className="profile-container">
+			<header>
+				<img src={ logoImg } alt="Be the Hero"/>
+				<span>Bem vindo, {ongName}</span>
+
+				<Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+				<button type="button" onClick={handleLogout}>
+					<FiPower size={18} color="#E02041" />
+				</button>
+			</header>
+			<h1>Casos Cadastrados</h1>
+			<ul>
+				{incidents.map(incident => (
+					<li key={incident.id}>
+						<strong>CASO:</strong>
+						<p>{incident.title}</p>
+						<strong>DESCRIÇÃO:</strong>
+						<p>{incident.description}</p>
+						<strong>VALOR:</strong>
+						<p>{Intl.NumberFormat('pt-BR', {style:'currency',currency:'BRL'}).format(incident.value)}</p>
+
+						<button type="button" onClick={() => handleDeleteIncident(incident.id)} >
+							<FiTrash2 size={20} color="#A8A8B3" />
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
